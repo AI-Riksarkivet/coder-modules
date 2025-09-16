83540443f811b7066511ff70db2233484366f638
Gabriel Borg <48671678+Borg93@users.noreply.github.com>
2025-09-16 13:39:05 +0200

    Update main.tf

--- langflow/main.tf
+++ langflow/main.tf
@@ -1,1 +1,104 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    coder = {
+      source  = "coder/coder"
+      version = ">= 2.5"
+    }
+  }
+}
 
+data "coder_workspace" "me" {}
+data "coder_workspace_owner" "me" {}
+
+variable "agent_id" {
+  type        = string
+  description = "The ID of a Coder agent."
+}
+
+variable "agent_name" {
+  type        = string
+  description = "The name of the coder_agent resource. (Only required if subdomain is false and the template uses multiple agents.)"
+  default     = null
+}
+
+variable "port" {
+  type        = number
+  description = "The port to run Langflow on."
+  default     = 7860
+}
+
+variable "subdomain" {
+  type        = bool
+  description = "Use subdomain for the app."
+  default     = false
+}
+
+variable "slug" {
+  type        = string
+  description = "The slug of the coder_app resource."
+  default     = "langflow"
+}
+
+variable "log_path" {
+  type        = string
+  description = "The path to log Langflow to."
+  default     = "/tmp/langflow.log"
+}
+
+variable "share" {
+  type    = string
+  default = "owner"
+  validation {
+    condition     = var.share == "owner" || var.share == "authenticated" || var.share == "public"
+    error_message = "Incorrect value. Please set either 'owner', 'authenticated', or 'public'."
+  }
+}
+
+variable "order" {
+  type        = number
+  description = "The order determines the position of app in the UI presentation."
+  default     = null
+}
+
+variable "group" {
+  type        = string
+  description = "The name of a group that this app belongs to."
+  default     = null
+}
+
+locals {
+  server_base_path = var.subdomain ? "" : format("/@%s/%s%s/apps/%s", data.coder_workspace_owner.me.name, data.coder_workspace.me.name, var.agent_name != null ? ".${var.agent_name}" : "", var.slug)
+  url              = "http://localhost:${var.po§rt}${local.server_base_path}"
+  healthcheck_url  = "http://localhost:${var.port}${local.server_base_path}/health"
+}
+
+resource "coder_script" "langflow" {
+  agent_id     = var.agent_id
+  display_name = "Langflow"
+  icon         = "/icon/python.svg"
+  run_on_start = true
+  script       = templatefile("${path.module}/run.sh", {
+    PORT             = var.port
+    SERVER_BASE_PATH = local.server_base_path
+    LOG_PATH         = var.log_path
+  })
+}
+
+resource "coder_app" "langflow" {
+  agent_id     = var.agent_id
+  slug         = var.slug
+  display_name = "Langflow"
+  url          = local.url
+  icon         = "/icon/python.svg"
+  subdomain    = var.subdomain
+  share        = var.share
+  order        = var.order
+  group        = var.group
+  
+  healthcheck {
+    url       = local.healthcheck_url
+    interval  = 10
+    threshold = 10
+  }
+}
